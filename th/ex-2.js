dfad02606eb0152f28ddea8d868d512542896d83
nichalada <nichaladasuphachayanon@gmail.com>
2025-04-07 05:44:23 +0700

    add answer to ex1-2

--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,40 @@
-const romanToInt = function (s) {
-  //Start coding here
+const romanToInt = function (string) {
+  const romanValue = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  let left = 0;
+
+  for (let right = 1; left < string.length; ) {
+    const current = romanValue[string[left]];
+    const next = romanValue[string[right]];
+
+    if (
+      next &&
+      (
+        (string[left] === "I" && (string[right] === "V" || string[right] === "X")) ||
+        (string[left] === "X" && (string[right] === "L" || string[right] === "C")) ||
+        (string[left] === "C" && (string[right] === "D" || string[right] === "M"))
+      )
+    ) {
+      result += next - current;
+      left = right + 1;
+      right = left + 1;
+    } else {
+      result += current;
+      left++;
+      right = left + 1;
+    }
+  }
+
+  return result;
 };
 
 const result1 = romanToInt("III"); // 3
